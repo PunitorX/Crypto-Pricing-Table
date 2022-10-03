6b047ff0be1ae66c93426189e6652dc83cffbe52
Robert Jones <jones.robertbrendan@gmail.com>
2022-10-03 01:17:57 -0500

    'Updated styles'

--- src/Coin.js
+++ src/Coin.js
@@ -2,27 +2,28 @@ import React from 'react'
 
 const Coin = ({name, image, sym, price, volume, change, high, low}) => {
   return (
-    <div className="coin-container text-white">
-        <div className="coin-row">
-            <div className="coin-intro">
-                <img src={image} alt="" className="coin-img" />
-                <h1 className="coin-name">{name}</h1>
-                <p className="coin-symbol">{sym}</p>
-            </div>
-            <div className="coin-data">
-                <div className="coin-price">${price}</div>
-                <div className="coin-volume">${volume.toLocaleString()}</div>
+    <div className="coin-container text-white text-[1.4rem] h-40" >
+        <table className="coin-row table-auto ">
+            
+            <tbody className="coin-data ">
+              <tr>
+                <img src={image} alt="" className="coin-img m-2 p-4 scale-50" />
+                <td className="coin-name p-8">{name}</td>
+                <td className="coin-symbol p-8">{sym}</td>
+                <td className="coin-price p-8">${price.toLocaleString()}</td>
+                <td className="coin-volume p-8">${volume.toLocaleString()}</td>
 
                 {change < 0 ? (
-                  <div className='text-red-900'>{change.toFixed(2)}%</div>
+                  <td className='text-red-900 p-8'>{change.toFixed(2)}%</td>
                 ) : (
-                  <div className='text-green-800'>{change.toFixed(2)}%</div>
+                  <td className='text-green-800 p-8'>{change.toFixed(2)}%</td>
                 )}
                 
-                <div>${high}</div>
-                <div>${low}</div>
-            </div>
-        </div>
+                <td className='p-8'>${high.toLocaleString()}</td>
+                <td className='p-8'>${low.toLocaleString()}</td>
+              </tr>
+            </tbody>
+        </table>
     </div>
   )
 }
